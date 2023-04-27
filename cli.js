7fa020c9c62a9b38eda71da915347a17a4d16503
Joeykim <joeykim@DESKTOP-3MMF113.localdomain>
2023-04-27 12:31:25 -0400

    Added missing quotation marks

--- cli.js
+++ cli.js
@@ -7,13 +7,13 @@ import fetch from 'node-fetch';
 const argv = minimist(process.argv.slice(2));
 
 if (argv.h) {
-	Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
     -e, -w        Longitude: E positive; W negative.
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.
+    -j            Echo pretty JSON from open-meteo API and exit.`);
     process.exit(0);
 }
 
